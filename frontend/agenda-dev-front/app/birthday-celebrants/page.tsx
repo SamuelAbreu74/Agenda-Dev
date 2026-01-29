0e4d7cd2975a397bb5f0582ed0edaee7df927918
Samuel <samuelabreudev@gmail.com>
2026-01-29 18:09:30 -0300

    feat: Adding a Title for Companies and Birthday Celebrants pages

--- frontend/agenda-dev-front/app/birthday-celebrants/page.tsx
+++ frontend/agenda-dev-front/app/birthday-celebrants/page.tsx
@@ -1,5 +1,12 @@
-export default function BirthdayCelebrants(){
-    return(
-        <h1 className="text-black">Página de Aniversariantes</h1>
+export default function BirthdayCelebrants() {
+    return (
+        <div>
+            <main className="flex-1 p-6 ml-3 flex flex-col gap-6">
+                <header className="mb-2">
+                    <h1 className="text-gray-900 text-4xl font-bold">Aniversariantes</h1>
+                    <p className="text-sm text-gray-600 mt-1"><i>Quinta-Feira, 18 de Maio</i></p>
+                </header>
+            </main>
+        </div>
     )
 }
