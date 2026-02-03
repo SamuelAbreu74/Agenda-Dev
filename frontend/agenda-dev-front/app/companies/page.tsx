1920f88b01f738ee1b46861a6072ee78890e22f8
Samuel <samuelabreudev@gmail.com>
2026-02-03 17:44:28 -0300

    feat: Adding CompanyComponent to Companies Page

--- frontend/agenda-dev-front/app/companies/page.tsx
+++ frontend/agenda-dev-front/app/companies/page.tsx
@@ -1,3 +1,5 @@
+import CompanyComponent from "@/components/companyComponents";
+
 export default function Companies(){
     return(
         <div>
@@ -6,6 +8,10 @@ export default function Companies(){
                     <h1 className="text-gray-900 text-4xl font-bold">Empresas</h1>
                     <p className="text-sm text-gray-600 mt-1"><i>Quinta-Feira, 18 de Maio</i></p>
                 </header>
+                <section className=" shadow-sm border border-gray-200 rounded-lg bg-white overflow-hidden hover:shadow-blue-200 hover:shadow-md transition">
+                    {/* Container de Empresa*/}
+                    <CompanyComponent/>
+                </section>
             </main>
         </div>
     )
