b64fa6bb7bebef87fe8af035fc89ec768000d6de
Samuel Abreu <samuelabreudev@gmail.com>
2026-03-17 17:39:28 -0300

    feat: DeletePerson Button-DONE

--- frontend/agenda-dev-front/components/addButton.tsx
+++ frontend/agenda-dev-front/components/addButton.tsx
@@ -2,7 +2,7 @@ export default function AddButton() {
 
 
     return (
-        <button className="flex gap-2 min-w-30 rounded-xl p-1 cursor-pointer bg-blue-600 hover:bg-blue-700" >
+        <button className="flex gap-2 min-w-30 rounded-xl p-1 cursor-pointer bg-blue-600 hover:bg-blue-700 text-white" >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
