fb066cc05e47af0e2bcdb6606a9fc2cb06369870
Samuel Abreu <samuelabreudev@gmail.com>
2026-03-25 14:00:58 -0300

    feat: New dto's errors messages

--- backend/src/person/dto/create-person.dto.ts
+++ backend/src/person/dto/create-person.dto.ts
@@ -3,29 +3,29 @@ import { Gender } from '../entities/person.entity';
 
 export class CreatePersonDto {
     @IsString()
-    @Length(3, 150)
+    @Length(3, 150, {message: "Não pode estar vazio ou com menos de 3 caracteres"})
     name: string;
 
-    @IsEmail()
-    @Length(5, 150)
+    @IsEmail({}, {message: "Precisa ser um email"})
+    @Length(5, 150, {message: "Não pode estar vazio ou com menos de 5 caracteres"})
     email: string;
 
     @IsString()
-    @Length(8, 20)
+    @Length(8, 20, {message: "Não pode estar vazio ou com menos de 8 caracteres"})
     whatsappContact: string;
 
     @IsString()
-    @Length(8, 20)
+    @Length(8, 20, {message: "Não pode estar vazio ou com menos de 8 caracteres"})
     cellContact: string;
 
     @IsString()
-    @Length(2, 100)
+    @Length(2, 100, {message: "Não pode estar vazio ou com menos de 2 caracteres"})
     profession: string;
 
-    @IsDateString()
+    @IsDateString({}, {message: "Precisa ser uma data válida"})
     birthDate: string;
 
-    @IsEnum(Gender)
+    @IsEnum(Gender, {message: "Precisa ser um dos seguintes valores: MASCULINO, FEMININO ou OUTRO"})
     gender: Gender
 
     @IsOptional()
@@ -34,7 +34,7 @@ export class CreatePersonDto {
 
     @IsOptional()
     @IsString()
-    @Length(2, 150)
+    @Length(2, 150,{message: "Não pode estar vazio ou com menos de 2 caracteres"})
     company?: string;
 
 
@@ -42,32 +42,32 @@ export class CreatePersonDto {
 
     // RUA
     @IsString()
-    @Length(10, 150)
+    @Length(10, 150, {message: "Não pode estar vazio ou com menos de 10 caracteres"})
     street: string;
 
     // NÚMERO
     @IsString()
-    @Length(1, 10)
+    @Length(1, 10, {message: "Não pode estar vazio"})
     number: string;
 
     // BAIRRO
     @IsString()
-    @Length(5, 100)
+    @Length(5, 100, {message: "Não pode estar vazio ou com menos de 5 caracteres"}) 
     neighborhood: string;
 
     // CIDADE
     @IsString()
-    @Length(5, 100)
+    @Length(5, 100, {message: "Não pode estar vazio ou com menos de 5 caracteres"})
     city: string;
 
     // ESTADO (UF)
     @IsString()
-    @Length(2)
+    @Length(2, 2, {message: "Não pode estar vazio ou com mais de 2 caracteres"})
     state: string;
     
     // CEP
     @IsString()
-    @Length(8)
+    @Length(8, 8, {message: "Não pode estar vazio ou diferente de 8 caracteres"})
     cep: string;
 
 
