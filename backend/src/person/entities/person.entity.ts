2221b77323c7238e309d3ebc9d8affda47ffbe7c
Samuel Abreu <samuelabreudev@gmail.com>
2026-04-04 16:11:51 -0300

    feat: Updating accountable-company relation

--- backend/src/person/entities/person.entity.ts
+++ backend/src/person/entities/person.entity.ts
@@ -1,78 +1,78 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-export enum Gender{
-    MALE = 'MASCULINO',
-    FEMALE = 'FEMININO',
-    OTHER = 'OUTRO',
+export enum Gender {
+  MALE = 'MASCULINO',
+  FEMALE = 'FEMININO',
+  OTHER = 'OUTRO',
 }
 
 @Entity()
 export class Person {
 
-    // ID
-    @PrimaryGeneratedColumn('uuid')
+  // ID
+  @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-    // NOME
-    @Column({ type: 'varchar', length: 150 })
+  // NOME
+  @Column({ type: 'varchar', length: 150 })
   name!: string;
 
-    // EMAIL
-    @Column({ type: 'varchar', length: 150, unique: true })
+  // EMAIL
+  @Column({ type: 'varchar', length: 150, unique: true })
   email!: string;
 
-    // CONTATO WHATSAPP
-    @Column({ type: 'varchar', length: 20 })
+  // CONTATO WHATSAPP
+  @Column({ type: 'varchar', length: 20 })
   whatsappContact!: string;
 
-    // CONTATO CELULAR
-    @Column({ type: 'varchar', length: 20 })
+  // CONTATO CELULAR
+  @Column({ type: 'varchar', length: 20 })
   cellContact!: string;
 
-    // =-=-=-= ENDEREÇO =-=-=-=
-    
-    //  RUA
-    @Column({ type: 'varchar', length: 150 })
+  // =-=-=-= ENDEREÇO =-=-=-=
+
+  //  RUA
+  @Column({ type: 'varchar', length: 150 })
   street!: string;
 
-    // NÚMERO
-    @Column({ type: 'varchar', length: 10 })
+  // NÚMERO
+  @Column({ type: 'varchar', length: 10 })
   number!: string;
 
-    // BAIRRO
-    @Column({ type: 'varchar', length: 100 })
+  // BAIRRO
+  @Column({ type: 'varchar', length: 100 })
   neighborhood!: string;
 
-    // CIDADE
-    @Column({ type: 'varchar', length: 100 })
+  // CIDADE
+  @Column({ type: 'varchar', length: 100 })
   city!: string;
 
-    // ESTADO (UF)
-    @Column({ type: 'varchar', length: 2 })
+  // ESTADO (UF)
+  @Column({ type: 'varchar', length: 2 })
   state!: string;
-    
-    // CEP
-    @Column({ type: 'varchar', length: 8 })
+
+  // CEP
+  @Column({ type: 'varchar', length: 8 })
   cep!: string;
 
-    // DATA DE NASCIMENTO
-    @Column({ type: 'date' })
+  // DATA DE NASCIMENTO
+  @Column({ type: 'date' })
   birthDate!: Date;
 
-    // SEXO
-    @Column({ type: 'enum', enum: Gender })
+  // SEXO
+  @Column({ type: 'enum', enum: Gender })
   gender!: Gender;
 
-    // PROFISSÃO
-    @Column({ type: 'varchar', length: 100 })
+  // PROFISSÃO
+  @Column({ type: 'varchar', length: 100 })
   profession!: string;
 
-    // EMPRESA QUE TRABALHA
-    @Column({ type: 'varchar', length: 150 })
+  // EMPRESA QUE TRABALHA
+  @Column({ type: 'varchar', length: 150 })
   company!: string;
 
-    // FOTO DE PERFIL (URL)
-    @Column({ type: 'varchar', length: 255, nullable: true })
+  // FOTO DE PERFIL (URL)
+  @Column({ type: 'varchar', length: 255, nullable: true })
   photoUrl!: string;
 
 
