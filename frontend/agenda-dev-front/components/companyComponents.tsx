82eef3f63c6a65111d41c9121847ac59265d310f
Samuel Abreu <samuelabreudev@gmail.com>
2026-04-04 18:05:38 -0300

    feat: EditCompanyFormModal Edit & Delete Logic - COMPLETED

--- frontend/agenda-dev-front/components/companyComponents.tsx
+++ frontend/agenda-dev-front/components/companyComponents.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react";
-
-
-
-
+import Link from "next/link";
 
 export interface Company {
     logoUrl: string;
@@ -23,8 +20,11 @@ export interface Company {
     cep: string;
 }
 
-export default function CompanyComponent() {
+interface CompanyComponentProps {
+    onEdit: (company: Company) => void;
+}
 
+export default function CompanyComponent({ onEdit }: CompanyComponentProps) {
     const [companies, setCompanies] = useState<Company[]>([]);
 
     // Chamada ao endpoint do backend (GET)
@@ -36,6 +36,26 @@ export default function CompanyComponent() {
         }
         fetchCompanies();
     }, [])
+
+
+    // Chamada ao endpoint do backend (DELETE)
+    const handleDeleteCompany = async (companyId: string) => {
+        if (!confirm("Tem certeza que deseja excluir?")) return;
+        try {
+            const response = await fetch(`http://localhost:3001/companies/${companyId}`, {
+                method: "DELETE"
+            })
+            const data = await response.json();
+            console.log(data);
+
+            if (response.ok) {
+                setCompanies((prev) => prev.filter(p => p.id !== companyId));
+                console.log("Deletado com sucesso");
+            }
+        } catch (error) {
+            console.error("Erro na requisição: ", error);
+        }
+    }
 
 
 
@@ -51,17 +71,19 @@ export default function CompanyComponent() {
                         </div>
                     </div>
                     <div className="flex gap-5">
-                        <div className="cursor-pointer flex p-1 min-w-30 gap-2 min-h-8 rounded bg-green-500 hover:bg-green-600 transition-colors"><img className="invert" width="24" height="24" src="https://img.icons8.com/material-outlined/24/whatsapp--v1.png" alt="whatsapp--v1" /> <h1 className="text-white">Whatsapp</h1></div>
-                        <div className="cursor-pointer flex p-1 w-8 gap-2 h-8  rounded-full items-center bg-gray-100 hover:bg-blue-200 transition">
+                        <Link href={`https://wa.me/55${company.whatsappContact}`} className="cursor-pointer flex p-1 min-w-30 gap-2 min-h-8 rounded bg-green-500 hover:bg-green-600 transition-colors"><img className="invert" width="24" height="24" src="https://img.icons8.com/material-outlined/24/whatsapp--v1.png" alt="whatsapp--v1" /> <h1 className="text-white">Whatsapp</h1></Link>
+                        {/* Botão de Editar */}
+                        <button className="cursor-pointer flex p-1 w-8 gap-2 h-8  rounded-full items-center bg-gray-100 hover:bg-blue-200 transition" onClick={() => onEdit(company)}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="size-6 stroke-gray-700 hover:stroke-blue-700 transition-colors">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                             </svg>
-                        </div>
-                        <div className="cursor-pointer flex p-1 w-8 gap-2 h-8  rounded-full items-center bg-gray-100 hover:bg-red-200 transition">
+                        </button>
+                        {/* Botão de Deletar */}
+                        <button className="cursor-pointer flex p-1 w-8 gap-2 h-8  rounded-full items-center bg-gray-100 hover:bg-red-200 transition" onClick={() => handleDeleteCompany(company.id)}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 stroke-gray-700 hover:stroke-red-500 transition-colors">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
-                        </div>
+                        </button>
                     </div>
                 </div>
             ))
---
+++ frontend/agenda-dev-front/components/editCompanyFormModal.tsx
@@ -0,0 +1,277 @@
+'use client'
+import { Company } from "./companyComponents";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+interface EditCompanyFormModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    company: Company | null;
+    onUpdate: () => void;
+}
+
+interface CompanyFormData {
+    logoUrl: string;
+    fantasyName: string;
+    companyName: string;
+    email: string;
+    cnpj: string;
+    accountable: string;
+    cellContact: string;
+    whatsappContact: string;
+    fixedContact: string;
+    street: string;
+    number: string;
+    neighborhood: string;
+    city: string;
+    state: string;
+    cep: string;
+}
+
+interface PersonProps {
+    id: string;
+    name: string;
+}
+
+export default function EditcompanyFormModal({ isOpen, onClose, company, onUpdate }: EditCompanyFormModalProps) {
+    const [persons, setPersons] = useState<PersonProps[]>([]);
+
+    // Buscando Pessoas para listar no atributo de responsáveis
+    useEffect(() => {
+        fetch("http://localhost:3001/persons")
+            .then((res) => res.json())
+            .then((data) => {
+                setPersons(data.all_persons)
+                if (data.all_persons.length > 0) {
+                    setFormData(prev => ({ ...prev, accountable: data.all_persons[0].name }));
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao carregar pessoas ", error);
+            });
+    }, []);
+
+
+    useEffect(() => {
+        if (company && isOpen) {
+            setFormData({
+                logoUrl: company.logoUrl || '',
+                fantasyName: company.fantasyName || '',
+                companyName: company.companyName || '',
+                email: company.email || '',
+                cnpj: company.cnpj || '',
+                accountable: typeof company.accountable === 'object'
+                    ? (company.accountable as any).id
+                    : company.accountable || '',
+                cellContact: company.cellContact || '',
+                whatsappContact: company.whatsappContact || '',
+                fixedContact: company.fixedContact || '',
+                street: company.street || '',
+                number: company.number || '',
+                neighborhood: company.neighborhood || '',
+                city: company.city || '',
+                state: company.state || '',
+                cep: company.cep || ''
+            });
+        }
+    }, [company, isOpen]);
+
+
+
+    // Criando estado para os dados
+    const [formData, setFormData] = useState<CompanyFormData>({
+        logoUrl: '',
+        fantasyName: '',
+        companyName: '',
+        email: '',
+        cnpj: '',
+        accountable: '',
+        cellContact: '',
+        whatsappContact: '',
+        fixedContact: '',
+        street: '',
+        number: '',
+        neighborhood: '',
+        city: '',
+        state: '',
+        cep: ''
+    });
+
+
+    if (!isOpen) return null;
+
+    // Funçao para atualizar o estado ao preencher o formulario
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }))
+    }
+
+    // Funçao geral que envia os dados
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        if (!company) return;
+        // Requisição PATCH na API para Editar a Pessoa.
+        try {
+            const response = await fetch(`http://localhost:3001/companies/${company.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                onUpdate(); // Atualiza a Lista
+                onClose(); // Fecha o Modal
+            }
+
+        } catch (error) {
+            console.error("Erro na requisiçao: ", error);
+        }
+    }
+
+    const handleLogoUpdate = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file || !company?.id) return;
+
+        // Define um nome único para o arquivo
+        const fileExt = file.name.split('.').pop();
+        const fileName = `${company.id}-${Math.random()}.${fileExt}`;
+        const filePath = `logos/${fileName}`;
+
+        // faz Upload para o Supabase Storage
+        const { error: uploadError } = await supabase.storage
+            .from('logos')
+            .upload(filePath, file);
+
+        if (uploadError) {
+            console.error('Erro no upload:', uploadError);
+            return;
+        }
+
+        // Pega a URL pública da imagem 
+        const { data: { publicUrl } } = supabase.storage
+            .from('logos')
+            .getPublicUrl(filePath);
+
+        // chama API que faz o PATCH no banco
+        const response = await fetch(`http://localhost:3001/companies/${company.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ logoUrl: publicUrl }), // Enviando a URL real do Supabase
+        });
+
+        if (response.ok) {
+            setFormData(prev => ({ ...prev, logoUrl: publicUrl }));
+        }
+    };
+
+
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4">
+            <div className="flex flex-col w-full max-w-3xl max-h-[90vh] overflow-y-auto border-2 rounded-2xl border-black shadow-md shadow-blue-200 p-6 bg-blue-950 text-white">
+
+                {/* HEADER */}
+                <div className="flex items-center justify-between pb-6 border-b border-blue-900/50 mb-4">
+                    <h1 className="text-xl md:text-2xl font-bold">Adicione uma nova Empresa:</h1>
+                    <button onClick={onClose} className="cursor-pointer hover:scale-110 transition-transform">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-7 text-white hover:text-blue-400">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </button>
+                </div>
+
+                {/* FORMULÁRIO EM GRID */}
+                <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
+
+                    {/* DADOS PESSOAIS */}
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="fantasyName">Nome Fantasia: </label>
+                        <input onChange={handleChange} name="fantasyName" value={formData.fantasyName} type="text" placeholder="Cacau Show" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="companyName" >Razão Social: </label>
+                        <input onChange={handleChange} name="companyName" value={formData.companyName} type="text" placeholder="IBAC – Indústria Brasileira de Alimentos e Chocolates Ltda" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="email" >Email: </label>
+                        <input onChange={handleChange} name="email" value={formData.email} type="text" placeholder="empresa123@gmail.com" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="cnpj" >CNPJ: </label>
+                        <input onChange={handleChange} name="cnpj" value={formData.cnpj} type="text" placeholder="00.000.000/0000-00" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="accountable" >Responsável: </label>
+                        <select onChange={handleChange} name="accountable" value={formData.accountable} className="w-full p-1 border rounded text-white">
+                            {persons.length === 0 && <option value="">Carregando...</option>}
+                            {persons.map((person) => (
+                                <option key={person.id} value={person.id} className="bg-blue-950 text-white">
+                                    {person.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="contact">Contato (Celular): </label>
+                        <input onChange={handleChange} name="cellContact" value={formData.cellContact} type="tel" placeholder="(00) 9 0000-0000" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="contact">Contato (Whatsapp): </label>
+                        <input onChange={handleChange} name="whatsappContact" value={formData.whatsappContact} type="tel" placeholder="(00) 9 0000-0000" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="contact">Contato (Fixo): </label>
+                        <input onChange={handleChange} name="fixedContact" value={formData.fixedContact} type="tel" placeholder="(00) 9 0000-0000" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2 md:col-span-2">
+                        <label htmlFor="profile_pic" >Logo da Empresa: </label>
+                        <input name="logoUrl" onChange={handleLogoUpdate} type="file" accept="image/*" className="w-full p-1 border rounded text-white file:mr-4 file:py-1 file:px-4 file:rounded-full file:border-0 file:text-sm file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100" />
+                        {formData.logoUrl && (
+                            <p className="text-xs text-blue-300">Logo atual: {formData.logoUrl.split('/').pop()}</p>
+                        )}
+                    </div>
+                    {/* DADOS DE ENDEREÇO */}
+                    <h2 className="text-2xl md:col-span-2 mt-4 border-b border-blue-800 pb-2">Endereço</h2>
+
+                    <div className="flex flex-col gap-2 md:col-span-1">
+                        <label htmlFor="street" >Rua: </label>
+                        <input onChange={handleChange} name="street" value={formData.street} type="text" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="number" >Número: </label>
+                        <input onChange={handleChange} name="number" value={formData.number} type="text" className="w-full md:max-w-20 p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="neighborhood" >Bairro: </label>
+                        <input onChange={handleChange} name="neighborhood" value={formData.neighborhood} type="text" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="city" >Cidade: </label>
+                        <input onChange={handleChange} name="city" value={formData.city} type="text" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="state" >Estado(UF): </label>
+                        <input onChange={handleChange} name="state" value={formData.state} type="text" className="w-full p-1 border rounded text-white" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="cep" >CEP: </label>
+                        <input onChange={handleChange} name="cep" value={formData.cep} type="text" className="w-full p-1 border rounded text-white" />
+                    </div>
+                </form>
+
+                {/* BOTÃO DE AÇÃO (OPCIONAL) */}
+                <div className="flex justify-end gap-4 mt-4">
+                    <button onClick={onClose} className="px-4 py-2 bg-red-600 cursor-pointer rounded-lg hover:bg-red-700 transition-colors">Cancelar</button>
+                    <button onClick={handleSubmit} type="submit" className="px-4 py-2 bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-700 transition-colors" >Salvar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
